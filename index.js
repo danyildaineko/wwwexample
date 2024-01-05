572500b97733f0c130407ea2da83614a4947b500
danyil daineko <50742042+danyildaineko@users.noreply.github.com>
2024-01-05 02:30:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+// script.js
+window.onload = function () {
+  console.log("Hello World!");
+  var savedTexts = JSON.parse(localStorage.getItem('savedTexts'));
+  if (savedTexts) {
+    savedTexts.forEach(text => {
+      addToTable(text);
+    });
+  }
+};
+
+function saveText() {
+  var text = document.getElementById('text-input').value;
+  var savedTexts = JSON.parse(localStorage.getItem('savedTexts')) || [];
+  savedTexts.push(text);
+  localStorage.setItem('savedTexts', JSON.stringify(savedTexts));
+  addToTable(text);
+  console.log('Saved Text: ', text);
+}
+
+function addToTable(text) {
+  var table = document.getElementById('text-table');
+  var row = table.insertRow(-1); // Append new row to the end
+  var cell = row.insertCell(0);
+  cell.innerHTML = text;
+}
 
